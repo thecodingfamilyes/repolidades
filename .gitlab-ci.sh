45b08ea9ffb32f7268f100f8f28ace222f76c52f
Antonio Gázquez <piticonejo@gmail.com>
2017-08-11 16:00:40 +0000

    add gitlabci

---
+++ .gitlab-ci.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eo pipefail
+
+# Install php libraries.
+echo "Start the update and the install"
+composer config -g github-oauth.github.com $gitHubKey
+composer install --no-interaction --optimize-autoloader
+
+# Copy over testing configuration.
+cp .env.testing .env
+
+# Generate an application key. Re-cache.
+echo "Run artisan"
+php artisan key:generate
+php artisan optimize
+php artisan config:cache
+
+# Run database migrations.
+echo "Run migration & Seeds"
+php artisan migrate --seed
+
+# Run the tests.
+echo "Run the tests"
+php vendor/bin/phpunit --colors
