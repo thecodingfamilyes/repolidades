ce0afb53ee919cfd3aaa4644bf1a5d278a6314e8
cso1992 <csoon1992@gmail.com>
2017-08-12 16:35:18 +0200

    fix

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,3 @@
-{{Request::path()}}
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 	<a class="navbar-brand" href="#">
 		<img src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
