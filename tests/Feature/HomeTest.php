20e01999364d4ad86514d51ad5d8a283f347cb8c
cso1992 <csoon1992@gmail.com>
2017-08-12 13:46:56 +0200

    adding home

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class HomeTest extends TestCase
+{
+    /** @test */
+    public function display_sections_buttons()
+    {
+    	$response = $this->get('/');
+
+      $response->assertStatus(200);
+      $response->assertSee('Repostería');
+      $response->assertSee('Manualidades');
+    }
+}
