36f515cfb099b0286b0abeb503f8f49407cf0eb9
cso1992 <csoon1992@gmail.com>
2017-08-15 11:50:47 +0200

    styles

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -6,19 +6,32 @@
 	<h1 class="text-center">{{ ucfirst($title) }}</h1>
 	<p class="text-center px-3">En esta sección podrás encontrar una gran variedad de productos de repostería 100% caseros.</p>
 	<div class="catalog-wrapper mw-md-75">
-		<div class="card-columns">
-			@foreach ($data as $product)
-				<div class="card">
-					<img class="card-img-top" src="http://4.bp.blogspot.com/-W3leNQisv_k/T-rl5Vb09fI/AAAAAAAAAkA/1C-16fq2tGQ/s1600/tarta+de+queso.pngs" alt="Card image cap">
-					<div class="card-body">
-						<h4 class="card-title">{{ $product->name }}</h4>
-						<p class="card-text">{{ $product->reference }}</p>
-						<p class="card-text">{{ $product->description }}</p>
-						<p class="card-text"><small class="text-muted">{{ $product->price }}</small></p>
-						<p class="card-text"><small class="text-muted">{{ $product->category }}</small></p>
+		@php
+			$columns = 2;
+		@endphp
+
+		@foreach ($data->chunk($columns) as $row)
+			<div class="card-deck mb-3">
+				@foreach ($row as $product)
+					<div class="card">
+						<img class="card-img-top" src="http://4.bp.blogspot.com/-W3leNQisv_k/T-rl5Vb09fI/AAAAAAAAAkA/1C-16fq2tGQ/s1600/tarta+de+queso.pngs" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">{{ $product->name }}</h4>
+							<p class="card-text">{{ $product->reference }}</p>
+							<p class="card-text">{{ $product->description }}</p>
+							<p class="card-text"><small class="text-muted">{{ $product->price }}</small></p>
+							<p class="card-text"><small class="text-muted">{{ $product->category }}</small></p>
+
+							<a href="/contacto" class="btn btn-outline-primary">¡Lo quiero!</a>
+						</div>
 					</div>
-				</div>
-			@endforeach
-		</div>
+					@if ($row->count() < $columns)
+						@for ($i = 0; $i < $columns - $row->count(); $i++)
+							<div class="card border-0"></div>
+						@endfor
+					@endif
+				@endforeach
+			</div>
+		@endforeach
 	</div>
 @endsection
